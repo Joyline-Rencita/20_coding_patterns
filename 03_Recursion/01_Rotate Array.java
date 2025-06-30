bfb5dce7277f6396cef1a292dd890e6df0c0c3e9
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-30 20:39:38 +0530

    Update 01_Rotate Array.java

--- 03_Recursion/01_Rotate Array.java
+++ 03_Recursion/01_Rotate Array.java
@@ -4,7 +4,7 @@ class Solution {
     public void rotate(int[] nums, int k) {
         this.nums = nums;
         int n = nums.length;
-        k %= n;
+        k %= n;   //  k = k%n
         reverse(0, n - 1);
         reverse(0, k - 1);
         reverse(k, n - 1);
@@ -12,9 +12,9 @@ class Solution {
 
     private void reverse(int i, int j) {
         for (; i < j; ++i, --j) {
-            int t = nums[i];
+            int temp = nums[i];
             nums[i] = nums[j];
-            nums[j] = t;
+            nums[j] = temp;
         }
     }
 }
