47fc04aa176a095846104e03a29899b7e0e3fefc
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-12 22:44:27 +0530

    Update 07_Sum Root to Leaf Numbers.java

--- 04_DFS/07_Sum Root to Leaf Numbers.java
+++ 04_DFS/07_Sum Root to Leaf Numbers.java
@@ -1,13 +1,6 @@
-You are given the root of a binary tree containing digits from 0 to 9 only.
-
-Each root-to-leaf path in the tree represents a number.
-
+You are given the root of a binary tree containing digits from 0 to 9 only. Each root-to-leaf path in the tree represents a number.
 For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
-Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
-
-A leaf node is a node with no children.
-
- 
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer. A leaf node is a node with no children.
 
 Example 1:
 Input: root = [1,2,3]
@@ -16,8 +9,8 @@ Explanation:
 The root-to-leaf path 1->2 represents the number 12.
 The root-to-leaf path 1->3 represents the number 13.
 Therefore, sum = 12 + 13 = 25.
-Example 2:
 
+Example 2:
 Input: root = [4,9,0,5,1]
 Output: 1026
 Explanation:
@@ -26,13 +19,10 @@ The root-to-leaf path 4->9->1 represents the number 491.
 The root-to-leaf path 4->0 represents the number 40.
 Therefore, sum = 495 + 491 + 40 = 1026.
  
-
 Constraints:
-
 The number of nodes in the tree is in the range [1, 1000].
 0 <= Node.val <= 9
 The depth of the tree will not exceed 10.
-
 
 /**
  * Definition for a binary tree node.
@@ -49,6 +39,7 @@ The depth of the tree will not exceed 10.
  *     }
  * }
  */
+ 
 class Solution {
     public int sumNumbers(TreeNode root) {
         return dfs(root, 0);
