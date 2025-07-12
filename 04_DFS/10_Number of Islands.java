e4ff28716b70942586beb528bfd627918516600b
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-12 22:46:40 +0530

    Update 10_Number of Islands.java

--- 04_DFS/10_Number of Islands.java
+++ 04_DFS/10_Number of Islands.java
@@ -3,32 +3,18 @@ An island is surrounded by water and is formed by connecting adjacent lands hori
 by water.
 
 Example 1:
-
-Input: grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
+Input: grid = [ ["1","1","1","1","0"], ["1","1","0","1","0"], ["1","1","0","0","0"], ["0","0","0","0","0"] ]
 Output: 1
-Example 2:
 
-Input: grid = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]
+Example 2:
+Input: grid = [ ["1","1","0","0","0"], ["1","1","0","0","0"], ["0","0","1","0","0"], ["0","0","0","1","1"] ]
 Output: 3
  
-
 Constraints:
-
 m == grid.length
 n == grid[i].length
 1 <= m, n <= 300
 grid[i][j] is '0' or '1'.
-
 
 class Solution {
     private char[][] grid;
